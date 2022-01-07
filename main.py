9f78eb0a57267357e8ef1b8f78e86889e69e5779
Krishnan Shankar <krishnans2006@gmail.com>
2022-01-07 22:30:45 +0000

    Fix spacing

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ import requests
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
     return "ChessCord Webhooks is up and running..."
+
 
 @app.route("/heroku", methods=["POST"])
 def heroku():
@@ -51,6 +53,7 @@ def heroku():
     url = "https://discord.com/api/webhooks/928901900286439465/" + "OB1eH1lPd23USGJLRJdGlN-mdzPUB69X1NN-X81E953b9IpnhqQEbj3eNnxbWxvz7q7A"
     requests.post(url, json=data)
     return ""
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
