b25a02a6733b1fecbfc0a62b8e44579806b5d9c3
Krishnan Shankar <krishnans2006@gmail.com>
2022-01-09 05:46:30 +0000

    Remove unnecessary imports pt. 2

--- utils.py
+++ utils.py
@@ -1,38 +1,3 @@
-from typing import Optional, List
-
-import discord
-from discord.utils import utcnow
-
-
-def create_embed(title: Optional[str] = None,
-                 description: Optional[str] = None,
-                 author: Optional[discord.User] = None,
-                 special_author: Optional[List[str]] = None,
-                 fields: Optional[list] = None,
-                 image: Optional[str] = None,
-                 thumbnail: Optional[str] = "https://images.discordapp.net/avatars/811760473422823465/4d2e470acfc0be5ce75fbc23b406c6ce.png?size=1024",
-                 color: Optional[hex] = 0xbd6f29) -> discord.Embed:
-    if title:
-        embed = discord.Embed(title=title, color=color)
-    else:
-        embed = discord.Embed(color=color)
-    if description:
-        embed.description = description
-    if author:
-        embed.set_author(name=author.name, icon_url=author.display_avatar.url)
-    elif special_author:
-        embed.set_author(name=special_author[0], icon_url=special_author[1])
-    if fields:
-        for field in fields:
-            embed.add_field(name=field[0], value=field[1], inline=field[2] if len(field) > 2 else False)
-    if image:
-        embed.set_image(url=image)
-    if thumbnail:
-        embed.set_thumbnail(url=thumbnail)
-    embed.timestamp = utcnow()
-    return embed
-
-
 def parse_heroku(f):
     if f.get("resource") == "build":
         c = None
