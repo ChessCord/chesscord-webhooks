110146407f0c36a0f0237a9ebb0b741375b146eb
Krishnan Shankar <krishnans2006@gmail.com>
2022-01-09 05:44:50 +0000

    Fix requirements + remove unnecessary imports

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-from threading import Thread
-import os
-
-from dotenv import load_dotenv
-
 from app import app
-
-load_dotenv()
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
